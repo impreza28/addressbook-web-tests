88cdd0e255c49681e2f638ce1b36f6121af132e8
Валерия Русакова <rusakovalera@gmail.com>
2022-03-20 17:24:22 +0300

    Дз11: реализовать тесты для проверки информации на главной странице

--- addressbook-web-tests/appmanager/ContactHelper.cs
+++ addressbook-web-tests/appmanager/ContactHelper.cs
@@ -127,16 +127,27 @@ namespace addressbook_web_tests
             //string middleName = driver.FindElement(By.Name("middlename")).GetAttribute("value");
             string lastName = driver.FindElement(By.Name("lastname")).GetAttribute("value");
             string address = driver.FindElement(By.Name("address")).GetAttribute("value");
+
             string homePhone = driver.FindElement(By.Name("home")).GetAttribute("value");
             string mobilePhone = driver.FindElement(By.Name("mobile")).GetAttribute("value");
             string workPhone = driver.FindElement(By.Name("work")).GetAttribute("value");
+            //string fax = driver.FindElement(By.Name("fax")).GetAttribute("value");
+
+            string email1 = driver.FindElement(By.Name("email")).GetAttribute("value");
+            string email2 = driver.FindElement(By.Name("email2")).GetAttribute("value");
+            string email3 = driver.FindElement(By.Name("email3")).GetAttribute("value");
+
 
             return new ContactData(firstName, lastName)
             {
                 Address = address,
                 HomePhone = homePhone,
                 MobilePhone = mobilePhone,
-                WorkPhone = workPhone
+                WorkPhone = workPhone,
+                //Fax = fax,
+                Email1 = email1,
+                Email2 = email2,
+                Email3 = email3
             };
         }
 
@@ -145,15 +156,17 @@ namespace addressbook_web_tests
             manager.Navigator.OpenHomePage();
             IList<IWebElement> cells = driver.FindElements(By.Name("entry"))[index]
                                            .FindElements(By.TagName("td"));
-            string firstname = cells[1].Text;
-            string lastname = cells[2].Text;
+            string firstname = cells[2].Text;
+            string lastname = cells[1].Text;
             string address = cells[3].Text;
+            string allEmails = cells[4].Text;
             string allPhones = cells[5].Text;
 
             return new ContactData(firstname, lastname)
              {
                 Address = address,
-                AllPhones = allPhones
+                AllPhones = allPhones,
+                AllEmails = allEmails
             };
         }
 
